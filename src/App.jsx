2f225e0cfa8d42b5c3177abcf1fa32a9ba84edfe
Leonardo Gama <leogama09@gmail.com>
2023-06-22 15:56:51 -0300

    Created board

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Layout from "./components/Layout/Layout";
 import Calendar from "./pages/Calendar/Calendar";
+import BoardPage from "./pages/Board/Board";
 
 const App = () => {
   return <div id="dashboard">
@@ -9,8 +10,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout/>}>
 
-          <Route path="dashboard" element={<Dashboard />}/>
-          <Route path="calendar" element={<Calendar />}/>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="board" element={<BoardPage />} />
 
         </Route>
       </Routes>
